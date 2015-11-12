dfe3e738cb2d742087f3ca480b4e634ea51ff473
Obtuse <theobtuseautodidact@gmail.com>
2015-11-12 10:36:37 -0700

    headcount_analyst functional; tests passing

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,4 +1,5 @@
-require './lib/district'
+require './lib/district'    # ~> LoadError: cannot load such file -- ./lib/district
+require './lib/enrollment'
 require 'pry'
 
 class HeadcountAnalyst
@@ -22,15 +23,33 @@ class HeadcountAnalyst
     rate_variation.round(3)
   end
 
+  def find_kindergarten_participation_by_year_for_district(district)
+     de_repo.find_by_name(district).enrollment.kindergarten_participation
+  end
+
+  def kindergarten_participation_rate_variation_trend(district, hash_comparison)
+    comparison = hash_comparison.values[0].upcase
+    district_participation = find_kindergarten_participation_by_year_for_district(district)
+    comparison_participation = find_kindergarten_participation_by_year_for_district(comparison)
+    result = {}
+    district_participation.keys.map do | year|
+      #binding.pry
+      result[year] = (district_participation[year] / comparison_participation[year]).round(3)
+    end
+    result
+  end
 
 
 
-  # ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO') # => 0.766
-
-  # ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1') # => 1.234
-
-
-  # ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO') # => {2009 => 0.766, 2010 => 0.566, 2011 => 0.46 }
 
 
 end
+
+  # ha.kindergarten_participation_by_year_for_district('ACADEMY 20')
+
+# ~> LoadError
+# ~> cannot load such file -- ./lib/district
+# ~>
+# ~> /Users/lenny/.rvm/rubies/ruby-2.2.1/lib/ruby/site_ruby/2.2.0/rubygems/core_ext/kernel_require.rb:54:in `require'
+# ~> /Users/lenny/.rvm/rubies/ruby-2.2.1/lib/ruby/site_ruby/2.2.0/rubygems/core_ext/kernel_require.rb:54:in `require'
+# ~> /Users/lenny/turing/1module/projects/headcount/lib/headcount_analyst.rb:1:in `<main>'
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -106,4 +106,69 @@ class HeadcountAnalystTest < Minitest::Test
 
     assert_equal expected, ha.kindergarten_participation_rate_variation("Adams 20", :against => "Colorado")
   end
+
+  def test_it_finds_kindergarten_participation_by_district
+    d_repo = DistrictRepository.new
+    e_repo = EnrollmentRepository.new
+    e1 = Enrollment.new({
+      :name => "Adams 20",
+      :kindergarten_participation => {
+        2010 => 0.567,
+        2011 => 0.675,
+        2012 => 0.876
+      }
+    })
+    e2 = Enrollment.new({
+      :name => "Colorado",
+      :kindergarten_participation => {
+        2010 => 0.9,
+        2011 => 0.0,
+        2012 => 0.456
+      }
+    })
+    e_repo.add_records([e1, e2])
+    d_repo.load_repos({:enrollment => e_repo})
+    ha = HeadcountAnalyst.new(d_repo)
+
+    expected = {
+      2010 => 0.567,
+      2011 => 0.675,
+      2012 => 0.876
+    }
+
+
+    assert_equal expected, ha.find_kindergarten_participation_by_year_for_district("Adams 20")
+  end
+
+  def test_it_variation_between_years_of_enrollment_info
+    d_repo = DistrictRepository.new
+    e_repo = EnrollmentRepository.new
+    e1 = Enrollment.new({
+      :name => "ADAMS 20",
+      :kindergarten_participation => {
+        2010 => 0.567,
+        2011 => 0.675,
+        2012 => 0.876
+      }
+    })
+    e2 = Enrollment.new({
+      :name => "COLORADO",
+      :kindergarten_participation => {
+        2010 => 0.9,
+        2011 => 0.345,
+        2012 => 0.456
+      }
+    })
+    e_repo.add_records([e1, e2])
+    d_repo.load_repos({:enrollment => e_repo})
+    ha = HeadcountAnalyst.new(d_repo)
+
+    expected = {
+      2010 => 0.63,
+      2011 => 1.957,
+      2012 => 1.921
+    }
+
+    assert_equal expected, ha.kindergarten_participation_rate_variation_trend('ADAMS 20', :against => 'COLORADO')
+  end
 end
