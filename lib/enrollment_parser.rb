eaf532e073561196d62e206d7869a818a34b8f78
Brant Wellman <BrantWellman@gmail.com>
2015-11-14 14:30:04 -0700

    Enrollment Rep takes in two files and creates enrollments. Needs more testing.

--- lib/enrollment_parser.rb
+++ lib/enrollment_parser.rb
@@ -1,21 +1,14 @@
 require 'csv'
 require 'pry'
 class EnrollmentParser
-  attr_reader :formatted_rows, :enrollment_objects
-
-  def initialize
-    @formatted_rows = []
-    @enrollment_objects = []
-  end
-
   def parse(key, file)
-    @formatted_rows = []
+    formatted_rows = []
     handle = CSV.open(file, {:headers => true, header_converters: :symbol })
     handle.each do |row|
       fancy_row = {:name => row[:location].upcase, key => {row[:timeframe].to_i => row[:data].to_f.round(3)}}
-      @formatted_rows << fancy_row
+      formatted_rows << fancy_row
     end
-    format_nested_hashes(key)
+    format_nested_hashes(key, formatted_rows)
   end
 
   def group_to_nested_hash(group, key)
@@ -28,8 +21,8 @@ class EnrollmentParser
      nested_hash
   end
 
-  def format_nested_hashes(key)
-    @formatted_rows.group_by { |r| r[:name] }.values.map do |group|
+  def format_nested_hashes(key, formatted)
+    formatted.group_by { |r| r[:name] }.values.map do |group|
       group_to_nested_hash(group, key)
     end
   end
--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -13,75 +13,39 @@ class EnrollmentRepository
   def load_data(hash)
     parser = EnrollmentParser.new
     load_files = peel_hash_to_key_file(hash)
-
     load_files.each do |key, file|
-      binding.pry
-            district_enrollment_data_over_time = parser.parse(key, file)
-            create_enrollment(district_enrollment_data_over_time)
+      district_enrollment_data_over_time = parser.parse(key, file)
+      create_enrollments(district_enrollment_data_over_time)
     end
-
-    # parsed_files = load_files.map do |key, file|
-    #   parser.parse(key, file)
-    # end
-    # parsed_files
-    # merged_files(parsed_files)
-    # district_enrollment_data_over_time = merge_files(parsed_files)
-    # create_enrollment(district_enrollment_data_over_time)
-      # binding.pry
   end
-
-
-  # def merged_files(files)
-  #     grouped = parsed_files.flatten.group_by {|hash| hash[:name]}
-  #     x = Hash.new
-  #     merged = grouped.each do |k, v|
-  #       v.each do |hash|
-  #         x.merge!(hash)
-  #       end
-  #       x
-  #     end
-  # end
-
-  # [[:kindergarten, "./data/Kindergartners in full-day program.csv"],
-  #            [:high_school_graduation, "./data/High school graduation rates.csv"]]
 
   def peel_hash_to_key_file(hash)
     hash.values[0].to_a
   end
 
-
-# loaded_data.group_by { |hash| hash[:name] }.map { |dist_name, dist_hashes| dist_hashes.reduce(:merge)}
-
-
-  def create_enrollment(district_enrollment_array)
+  def create_enrollments(district_enrollment_array)
     district_enrollment_array.each do |hash_line|
-      @enrollments << Enrollment.new(hash_line)
-
+      create_enrollment(hash_line)
+      # @enrollments << Enrollment.new(hash_line)
     end
-    binding.pry
   end
 
-  # def check_for_pre_existing_enrollment
-  #   # if enrollment matching name in hashline does not exist
-  #   if find_by_name(hash_line[:name])
-  # # add data matching your key from hashline to matching instance varialbe on pre existing
-  #     find_by_name(hash_line[:name]).kindergarten = hash_line[:kindergarten]
-  #     #instance variable that matches key of incoming hashline = hashline[:key that matches instance that is not name on enrollment object]
+  def create_enrollment(hash_line)
+      # if enrollment matching name in hashline does not exist
+    if find_by_name(hash_line[:name])
+    # add data matching your key from hashline to matching instance varialbe on pre existing
+      #instance variable that matches key of incoming hashline = hashline[:key that matches
+      find_by_name(hash_line[:name]).high_school_graduation = hash_line[:high_school_graduation]
+      #  instance that is not name on enrollment object]
+    else
+      @enrollments << Enrollment.new(hash_line)
+    end
+
+  #   if enrollment matching name in hashline does not exist
+  #     then created new enrollment
   #   else
-  # end
-  # if enrollment matching name in hashline does not exist
-  #   then created new enrollment
-  # else
-      # add data matching your key from hashline to matching instance varialbe on pre existing enrollment
-  # end
-
-  # if enrollments.find_by_name(name) === hashline name
-  # if find_by_name(hash_line[:name])
-  #   # then append to that enrollment - don't create new one
-  #   # enrollment object - find key and set value to be equal to the instance varialbe of the enrollment object
-  #   find_by_name(hash_line[:name]). =
-  # # else
-
+  #       add data matching your key from hashline to matching instance varialbe on pre existing enrollment
+  end
 
   def add_records(records)
     @enrollments += records
@@ -94,11 +58,34 @@ class EnrollmentRepository
 
 end
 
+      # if enrollments.find_by_name(name) === hashline name
+      # if find_by_name(hash_line[:name])
+      #   # then append to that enrollment - don't create new one
+      #   # enrollment object - find key and set value to be equal to the instance varialbe of the enrollment object
+      #   find_by_name(hash_line[:name]). =
+      # # else
+
+
+
+  # [[:kindergarten, "./data/Kindergartners in full-day program.csv"],
+  #            [:high_school_graduation, "./data/High school graduation rates.csv"]]
+
+
+
+# loaded_data.group_by { |hash| hash[:name] }.map { |dist_name, dist_hashes| dist_hashes.reduce(:merge)}
+
+
+
+
+
+
+
+
 er = EnrollmentRepository.new
 er.load_data({
   :enrollment => {
-    :high_school_graduation => "./data/High school graduation rates.csv",
-    :kindergarten => "./data/Kindergartners in full-day program.csv"
+    :kindergarten => "./data/Kindergartners in full-day program.csv",
+    :high_school_graduation => "./data/High school graduation rates.csv"
   }
 })
 enrollment = er.find_by_name("Colorado")
@@ -106,4 +93,30 @@ enrollment = er.find_by_name("Colorado")
 # # puts enrollment.kindergarten_participation_in_year(2010)
 # # puts dr.find_by_name("ACADEMY 20")
 # # p dr.districts
-p enrollment
+p er.enrollments
+p er.enrollments.count
+
+
+
+
+
+#
+# parsed_files = load_files.map do |key, file|
+#   parser.parse(key, file)
+# end
+# parsed_files
+# merged_files(parsed_files)
+# district_enrollment_data_over_time = merge_files(parsed_files)
+# create_enrollment(district_enrollment_data_over_time)
+#   binding.pry
+
+# def merged_files(files)
+#     grouped = parsed_files.flatten.group_by {|hash| hash[:name]}
+#     x = Hash.new
+#     merged = grouped.each do |k, v|
+#       v.each do |hash|
+#         x.merge!(hash)
+#       end
+#       x
+#     end
+# end
